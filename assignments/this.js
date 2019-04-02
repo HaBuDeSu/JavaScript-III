5424228a374f97b0b9e303cf55f02fde18a898d3
HaBuDeSu <ruf.kenichi@gmail.com>
2019-04-01 21:23:11 -0500

    four principles of this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global - When working in global scope "this" binds to the window/console.
+* 2. Implicit - Implicit binding is when "this" is bound to an object automatically.
+* 3. New - The new binding is used when creating a new instance of an object.
+* 4. Explicit - Explicit binding is when you specify in the code what "this" is bound to.
 *
 * write out a code example of each explanation above
 */
